5df7b08f541567e8bbe5934ef5857ac630b06afd
mochapi <mochapi@hatena.ne.jp>
2018-02-11 23:15:05 +0900

    名刺

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var plumber = require("gulp-plumber");
+
+gulp.task('build-css', function() {
+  return gulp.src(['less/*.less', '!less/**/_*.less'])
+    .pipe(plumber())
+    .pipe(less()).pipe(gulp.dest(''));
+});
 
 gulp.task('default', function() {
-  console.log("Hello World!");
+  gulp.watch("less/*.less", ['build-css']);
 });
