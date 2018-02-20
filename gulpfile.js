469385c5b2108c0612a70781349a99ba477dd38b
mochapi <mochapi@hatena.ne.jp>
2018-02-20 18:53:07 +0900

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
 var less = require('gulp-less');
-var plumber = require("gulp-plumber");
+var plumber = require('gulp-plumber');
+var cssmin = require('gulp-cssmin');
 
-gulp.task('build-css', function() {
-  return gulp.src(['less/*.less', '!less/**/_*.less'])
-    .pipe(plumber())
-    .pipe(less()).pipe(gulp.dest(''));
+// lessコンパイル
+gulp.task('less', function() {
+    return gulp.src(['./less/*.less', '!./less/**/_*.less'])
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(''));
 });
 
+// gulpコマンド実行時に行われるタスク
 gulp.task('default', function() {
-  gulp.watch("less/*.less", ['build-css']);
+  gulp.watch("./less/*.less", ['less']);
 });
